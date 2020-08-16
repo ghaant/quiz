26aea2f64053d70ce779b6a4beb4abbaf3147859
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-08-16 19:45:07 +0200

    Routing specs.

--- spec/routing/question_responses_routing_spec.rb
+++ spec/routing/question_responses_routing_spec.rb
@@ -2,37 +2,8 @@ require "rails_helper"
 
 RSpec.describe QuestionResponsesController, type: :routing do
   describe "routing" do
-    it "routes to #index" do
-      expect(get: "/question_responses").to route_to("question_responses#index")
-    end
-
-    it "routes to #new" do
-      expect(get: "/question_responses/new").to route_to("question_responses#new")
-    end
-
-    it "routes to #show" do
-      expect(get: "/question_responses/1").to route_to("question_responses#show", id: "1")
-    end
-
-    it "routes to #edit" do
-      expect(get: "/question_responses/1/edit").to route_to("question_responses#edit", id: "1")
-    end
-
-
-    it "routes to #create" do
-      expect(post: "/question_responses").to route_to("question_responses#create")
-    end
-
-    it "routes to #update via PUT" do
-      expect(put: "/question_responses/1").to route_to("question_responses#update", id: "1")
-    end
-
-    it "routes to #update via PATCH" do
-      expect(patch: "/question_responses/1").to route_to("question_responses#update", id: "1")
-    end
-
-    it "routes to #destroy" do
-      expect(delete: "/question_responses/1").to route_to("question_responses#destroy", id: "1")
+    it "routes to #toggle" do
+      expect(post: "/questions/1/question_responses/1/toggle").to route_to("question_responses#toggle", question_id: "1", id: "1")
     end
   end
 end
