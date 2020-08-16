529f7ed0521ef01d7f4da060f0c5e542a1a13b4c
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-08-16 17:10:35 +0200

    Edit routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root to: 'quizzes#show'
 
-  resources :questions, only: [:show] do
+  resources :questions, only: [] do
     post '/question_responses/:id/toggle', to: 'question_responses#toggle', as: 'response_toggle'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
