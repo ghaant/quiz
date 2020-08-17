df5bca0d342145d57269fb3263929aac5bed1d6d
Anton Adigamov <AdigamovAntonAAA@gmail.com>
2020-08-17 12:09:12 +0200

    View specs.

--- spec/views/quizzes/show.html.erb_spec.rb
+++ spec/views/quizzes/show.html.erb_spec.rb
@@ -1,16 +1,57 @@
 require 'rails_helper'
 
 RSpec.describe "quizzes/show", type: :view do
-  before(:each) do
-    @quiz = assign(:quiz, Quiz.create!(
-      title: "Title",
-      finished: false
-    ))
+  let!(:response) { FactoryBot.create(:question_response, correct: true, checked: true) }
+
+  context 'when the quiz is in progress' do
+    before do
+      response.question.quiz.finished = false
+      @quiz = assign(:quiz, response.question.quiz)
+      render
+    end
+
+    it 'renders the quiz title' do
+      expect(rendered).to match(response.question.quiz.title)
+    end
+
+    it 'renders the question' do
+      expect(rendered).to match(response.question.text)
+    end
+
+    it 'renders the response' do
+      expect(rendered).to match(response.text)
+    end
+
+    it 'renders the checkbox' do
+      assert_select "*[@id='checked']"
+    end
+
+    it 'renders the Finish button' do
+      assert_select "body > form > button", text: 'Finish'
+    end
   end
 
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Title/)
-    # expect(rendered).to match(/false/)
+  context 'when the quiz is finished' do
+    before do
+      response.question.quiz.finished = true
+      @quiz = assign(:quiz, response.question.quiz)
+      render
+    end
+
+    it 'renders the quiz title' do
+      expect(rendered).to match(response.question.quiz.title)
+    end
+
+    it 'renders the heading' do
+      expect(rendered).to match('Your results')
+    end
+
+    it 'renders the result sentence' do
+      expect(rendered).to match('You got right')
+    end
+
+    it 'renders the Re-do button' do
+      assert_select "body > form > button", text: 'Re-do the quiz'
+    end
   end
 end
